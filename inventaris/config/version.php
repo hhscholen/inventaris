bee5b4e6583b2abe23fd40825c649a0ac1af0ab0
Merlin Deramaix <merlin.deramaix@hhscholen.be>
2021-06-25 12:51:40 +0200

    v1 - before data import

---
+++ inventaris/config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v5.1.7',
+  'full_app_version' => 'v5.1.7 - build 6136-gbc0c88781',
+  'build_version' => '6136',
+  'prerelease_version' => '',
+  'hash_version' => 'gbc0c88781',
+  'full_hash' => 'v5.1.7-25-gbc0c88781',
+  'branch' => 'master',
+);
